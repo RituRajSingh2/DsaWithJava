58c361b59f66f6b18550415fbcb3aa6de701bc9d
Ritu Raj Singh <rituraj34013@gmail.com>
2025-05-16 19:14:46 +0530

    to Commit input.java

--- src/com/Array/Input.java
+++ src/com/Array/Input.java
@@ -12,14 +12,15 @@ public class Input {
         arr[4]=78;
         arr[5]=43;
         System.out.println(arr[3]);
+//        second way to take input
+        int [] arr1={12,32,72,68,78,43};
 //        taking input using loop
         Scanner in=new Scanner(System.in);
         System.out.println("enter value:");
-        int i;
-        for(i=0; i<arr.length; i++){
+        for(int i=0; i<arr.length; i++){
             arr[i]=in.nextInt();
         }
-        for(i=0; i<arr.length; i++){
+        for(int i=0; i<arr.length; i++){
             System.out.println(arr+" ");
         }
     }
