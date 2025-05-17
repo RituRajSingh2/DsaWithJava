7bb7f674d7cddf09c04f346c05ed328046f35e14
Ritu Raj Singh <rituraj34013@gmail.com>
2025-05-17 20:52:23 +0530

    Program to learn how to Pass integers in arrays

--- src/com/Array/PassingInFunctions.java
+++ src/com/Array/PassingInFunctions.java
@@ -1,5 +1,15 @@
+//this is mutabilty of interger in java
+//String are not mutable in java
 package com.Array;
 import java.util.Arrays;
 public class PassingInFunctions {
-
+    public static void main(String[] args) {
+        int[] arr={21,34,234,56};
+        System.out.println("Arrays are:" +Arrays.toString(arr));
+        change(arr);
+        System.out.println("Arrays are:" +Arrays.toString(arr));
+    }
+    static void change(int[]nums){
+        nums[0]=99;
+    }
 }
