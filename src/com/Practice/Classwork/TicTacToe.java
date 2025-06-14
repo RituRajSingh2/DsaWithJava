0ebae09700889adf30d84faf77ec047148695cfc
Ritu Raj Singh <rituraj34013@gmail.com>
2025-06-14 14:58:05 +0530

    this is commit of @14/06/25

---
+++ src/com/Practice/Classwork/TicTacToe.java
@@ -0,0 +1,3 @@
+package com.Practice.Classwork;
+public class TicTacToe {
+}
--- src/com/Practice/week1/TwoSum.java
+++ src/com/Practice/week1/TwoSum.java
@@ -3,7 +3,7 @@ public class TwoSum {
     public static void main(String[] args) {
         int [] nums={2,7,11,15};
         int target=9;
-        int[] result=TwoSum( nums,target);
+        int[] result=TwoSum2( nums,target);
         if(result != null){
             System.out.println("Indices are:"+result[0]+","+result[1]);
         }
@@ -11,12 +11,12 @@ public class TwoSum {
             System.out.println("No such indices exist");
         }
     }
-    static int[]TwoSum(int [] nums,int target){
+    static int[]TwoSum2(int [] nums,int target){
         int n=nums.length;
         for (int i = 0; i < n; i++) {
             for (int j = i+1; j < n; j++) {
 //  == is used for comparison and = is is used for assignment
-                if(nums[i]==nums[j]){
+                if(nums[i]+nums[j]==target){
                     return new int[]{i,j};
                 }
             }
